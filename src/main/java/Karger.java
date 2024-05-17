af059246b26f344a1a97926bb143d5813a723e97
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-05-17 12:22:22 +0300

    Partial implementation of Karger's algorithm

--- src/main/java/Karger.java
+++ src/main/java/Karger.java
@@ -2,14 +2,27 @@ import graphs.Multigraph;
 
 public class Karger {
     private final int repeatTimes;
-    //private final Multigraph multigraph;
+    private final Multigraph multigraph;
 
     public Karger(int repeatTimes, Multigraph multigraph) {
         this.repeatTimes = repeatTimes;
-        //this.multigraph = new Multigraph(multigraph);
+        this.multigraph = new Multigraph(multigraph);
     }
 
-    public void run() {
+    public Multigraph run() {
+        Multigraph minCut = multigraph;
 
+        for (int i = 0; i < repeatTimes; ++i) {
+            Multigraph cut = contract(new Multigraph(multigraph));
+            // TODO: get edge count from graph
+            // TODO: update minCut if cut.edgeCount < minCut.edgeCut;
+        }
+
+        return minCut;
+    }
+
+    private Multigraph contract(Multigraph multigraph) {
+        // TODO: implement
+        return multigraph;
     }
 }
