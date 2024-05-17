d6f39530ea1dd4112a86948950ace6d437266236
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-05-17 11:51:53 +0300

    Karger.java constructor

---
+++ src/main/java/Karger.java
@@ -0,0 +1,15 @@
+import graphs.Multigraph;
+
+public class Karger {
+    private final int repeatTimes;
+    //private final Multigraph multigraph;
+
+    public Karger(int repeatTimes, Multigraph multigraph) {
+        this.repeatTimes = repeatTimes;
+        //this.multigraph = new Multigraph(multigraph);
+    }
+
+    public void run() {
+
+    }
+}
